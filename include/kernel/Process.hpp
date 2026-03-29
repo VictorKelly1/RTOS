02961a58f39574b08f23b55c2bd0c7bc72a658a5
Victor Kelly <vk561244@gmail.com>
2026-03-28 20:01:45 -0600

    First desing of Class Process added

---
+++ include/kernel/Process.hpp
@@ -0,0 +1,46 @@
+#ifndef PROCESS_HPP
+#define PROCESS_HPP
+
+#include <stddef.h>
+#include <stdint.h>
+
+class Process final {
+
+private:
+  using TaskFunction = void (*)();
+
+  enum class State : uint8_t { READY, RUNNING, BLOCKED };
+
+  static constexpr uint16_t STACK_SIZE{128};
+
+  static uint8_t PID;
+
+private:
+  TaskFunction m_task;
+
+  uint8_t m_stack[STACK_SIZE]; // static size of 128 words
+  uint8_t *m_sp;
+
+  State m_state;
+
+  uint8_t m_pid;
+  uint8_t m_priority;
+
+public:
+  // Constructor
+  Process() = delete;
+
+  Process(TaskFunction task, uint8_t priority);
+
+  // Getters and Setters
+  uint8_t *getSP() const { return m_sp; }
+  void setSP(uint8_t *sp) { m_sp = sp; }
+
+  uint8_t getPID() const { return m_pid; }
+  uint8_t getPriority() const { return m_priority; }
+
+  void setState(State state) { m_state = state; }
+  State getState() const { return m_state; }
+};
+
+#endif
---
+++ src/kernel/Process.cpp
@@ -0,0 +1,27 @@
+#include "kernel/Process.hpp"
+
+uint8_t Process::PID{0};
+
+Process::Process(TaskFunction task, uint8_t priority)
+    : m_task(task)
+    , m_state(State::READY)
+    , m_priority(priority) 
+{
+  m_pid = ++PID;
+
+  //Stack iniciatlization
+  m_sp = &m_stack[STACK_SIZE - 1];
+
+  uint16_t funcPtr = reinterpret_cast<uint16_t>(m_task); //Pointer to the task
+
+  *m_sp-- = (uint8_t)( funcPtr & 0xFF);                  //Write in m_sp adress, then decrement the adress and extract low bit  
+  *m_sp-- = (uint8_t)((funcPtr >> 8) & 0xFF);             
+
+  *m_sp-- = 0x80;                                        //Activate interruptioins 
+
+  for (uint8_t index {}; index < 32; ++index){           
+        *m_sp-- = 0x00;
+  }
+
+  ++m_sp;
+}
